703b39b869dfd3ccf0cc26d5ffc85d36df3b7650
roverflow <roverflow5@gmail.com>
2021-07-21 19:06:44 +0530

    Added ID SignUp Table
    Added iD field in signup table

--- src/pages/tables/Tables-2.js
+++ src/pages/tables/Tables-2.js
@@ -1,36 +1,46 @@
-import React, {useEffect, useState} from "react";
-import {firestore} from '../../Services/firebase'
+import React, { useEffect, useState } from "react";
+import { firestore } from "../../Services/firebase";
 import { Grid } from "@material-ui/core";
 import MUIDataTable from "mui-datatables";
 import PageTitle from "../../components/PageTitle";
 
 export default function Tables2() {
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     let isMounted = true;
     const fetchData = async () => {
       await firestore.collection("/SignUpForm").onSnapshot(async (snapshot) => {
         if (isMounted) {
-          setTableData([])
+          setTableData([]);
         }
         snapshot.forEach((snap) => {
           if (snap.exists) {
             if (isMounted) {
               let tempData = snap.data();
-              let finalTemp = [tempData.TypeValue, tempData.Name, tempData.PhoneNumber, tempData.Email, tempData.Subject, tempData.Message, tempData.curTime]
-              setTableData(prevState => [...prevState, finalTemp])
+              let finalTemp = [
+                tempData.formid,
+                tempData.TypeValue,
+                tempData.Name,
+                tempData.PhoneNumber,
+                tempData.Email,
+                tempData.Subject,
+                tempData.Message,
+                tempData.curTime,
+                tempData.curDate,
+              ];
+              setTableData((prevState) => [...prevState, finalTemp]);
             }
           }
-        })
-      })
-    }
+        });
+      });
+    };
     fetchData();
 
     return () => {
       isMounted = false;
-    }
-  }, [])
+    };
+  }, []);
   return (
     <>
       <PageTitle title="Sign Up Form" />
@@ -39,7 +49,17 @@ export default function Tables2() {
           <MUIDataTable
             title="Users Signed Up"
             data={tableData}
-            columns={["Type of User", "Name", "Phone Number", "Email", "Subject", "Message", "Form Time"]}
+            columns={[
+              "ID",
+              "Type of User",
+              "Name",
+              "Phone Number",
+              "Email",
+              "Subject",
+              "Message",
+              "Time",
+              "Date",
+            ]}
             options={{
               filterType: "checkbox",
             }}
